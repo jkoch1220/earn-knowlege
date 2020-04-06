cb74e1b6ae4b835eb2f6fb43f7c01a1664dea533
Julian Koch <j.koch@tarent.de>
2020-04-06 12:28:39 +0200

    Init

---
+++ earn-kowlege/src/main/kotlin/com/buissnes/earnkowlege/EarnKowlegeApplication.kt
@@ -0,0 +1,11 @@
+package com.buissnes.earnkowlege
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EarnKowlegeApplication
+
+fun main(args: Array<String>) {
+	runApplication<EarnKowlegeApplication>(*args)
+}
---
+++ earn-kowlege/src/test/kotlin/com/buissnes/earnkowlege/EarnKowlegeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.buissnes.earnkowlege
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EarnKowlegeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
